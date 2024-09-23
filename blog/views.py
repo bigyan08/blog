d4b83bfcb2b8bf5facfb2f46f4ae225b3a991787
Bigyan <bigyan8aryal@gmail.com>
2024-09-23 20:08:07 +0545

    user posts available

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView , DetailView, CreateView , UpdateView , DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
+from django.contrib.auth.models import User
 
 # Create your views here
 class PostListView(ListView):
@@ -11,6 +12,18 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted'] # here '-' sign changes the order of the posts from oldest2latest to latest2oldest
     paginate_by =5 #no. of posts per page
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by =5 #no. of posts per page
+    
+    def get_queryset(self):
+        username =get_object_or_404(User,username=self.kwargs.get('username'))
+        user = get_object_or_404(User, username=username)
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model= Post
