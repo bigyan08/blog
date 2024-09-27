2a99ce256b9646c79e547e4eb5b9cffdb4248412
Bigyan <bigyan8aryal@gmail.com>
2024-09-27 18:52:28 +0545

    deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,6 +16,16 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+
+env=environ.Env()
+
+environ.Env.read_env()
+
+
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -80,12 +90,24 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
+#postgreSQL database setup
+import dj_database_url
+
+DATABASES={
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
+
+
+
+
 
 
 # Password validation
