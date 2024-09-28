5cc93c03b1c71a07004520fea338a4f0961886b7
Bigyan <bigyan8aryal@gmail.com>
2024-09-28 15:01:08 +0545

    deployed

--- django_project/settings.py
+++ django_project/settings.py
@@ -150,6 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL="/media/"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
