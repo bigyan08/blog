da28f084d11d26d3db8807e6e7608ca6b42c4cbd
Bigyan <bigyan8aryal@gmail.com>
2024-10-28 22:22:31 +0545

    setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env()
 SECRET_KEY =os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
