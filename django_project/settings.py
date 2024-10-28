08630e83b1a4ad82da983e1e51b5b3a8282c2b2a
Bigyan <bigyan8aryal@gmail.com>
2024-10-28 21:23:19 +0545

    new

--- django_project/settings.py
+++ django_project/settings.py
@@ -92,20 +92,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
-#postgreSQL database setup
-import dj_database_url
 
-DATABASES={
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
 
 
 
